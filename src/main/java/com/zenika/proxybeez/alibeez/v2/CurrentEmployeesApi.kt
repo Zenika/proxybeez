be1716188264b7a11167749b1297fdc72ff6bb54
Hugo Wood <hx4.5@free.fr>
2018-12-22 00:26:46 +0100

    refactor

--- src/main/java/com/zenika/proxybeez/alibeez/v2/CurrentEmployeesApi.kt
+++ src/main/java/com/zenika/proxybeez/alibeez/v2/CurrentEmployeesApi.kt
@@ -3,26 +3,29 @@ package com.zenika.proxybeez.alibeez.v2
 import com.codahale.metrics.annotation.Timed
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.swagger.annotations.Api
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpClientErrorException
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
 
 @Api(tags = ["User", "Employee"], description = "API to get current employees")
 @RestController
 @RequestMapping("/api/v2")
 class CurrentEmployeesResource(
     val alibeezProperties: AlibeezProperties,
-    val objectMapper: ObjectMapper
+    val fetcher: AlibeezCurrentEmployeesFetcher
 ) {
     @GetMapping("/current-employees")
     @Timed
     fun get(): ResultForAllInstances =
         alibeezProperties.instances
             .filter { it.value.key.isNotEmpty() }
-            .mapValues { getFromInstance(it.value) }
+            .mapValues { buildUri(it.value) }
+            .mapValues { fetchCurrentEmployees(it.value) }
             .mapValues { (instance, result) ->
                 alibeezProperties.instances[instance]?.defaults?.let { applyDefaults(result, it) } ?: result
             }
@@ -31,8 +34,8 @@ class CurrentEmployeesResource(
                 resultForAll.merge(resultForOne, instance)
             }
 
-    private fun getFromInstance(config: AlibeezInstanceProperties): ResultForOneInstance {
-        val uri = UriComponentsBuilder.fromHttpUrl(config.baseUrl)
+    private fun buildUri(config: AlibeezInstanceProperties): URI =
+        UriComponentsBuilder.fromHttpUrl(config.baseUrl)
             .pathSegment("users")
             .queryParam("key", config.key)
             .queryParam("fields", config.fields.joinToString(","))
@@ -40,20 +43,15 @@ class CurrentEmployeesResource(
             .build()
             .encode()
             .toUri()
-        val restTemplate = RestTemplate()
-        val response = try {
-            restTemplate.getForObject(uri, AlibeezResponse::class.java)
-        } catch (ex: HttpClientErrorException) {
-            val error = objectMapper.readValue(ex.responseBodyAsString, AlibeezError::class.java)
-            return ResultForOneInstance(
-                error = ErrorDto(
-                    statusCode = ex.statusCode.value(),
-                    statusText = ex.statusText,
-                    errorCode = error.errorCode,
-                    errorMessage = error.errorMessage
-                )
-            )
-        }
+
+    private fun fetchCurrentEmployees(uri: URI): ResultForOneInstance =
+        fetcher.fetch(
+            uri,
+            ::mapSuccess,
+            ::mapError
+        )
+
+    private fun mapSuccess(response: AlibeezResponse): ResultForOneInstance {
         val employees = response.result.map { user ->
             EmployeeDto(
                 id = user.uuid,
@@ -72,6 +70,16 @@ class CurrentEmployeesResource(
         return ResultForOneInstance(employees, employees.size)
     }
 
+    private fun mapError(exception: HttpClientErrorException, error: AlibeezError) =
+        ResultForOneInstance(
+            error = ErrorDto(
+                statusCode = exception.statusCode.value(),
+                statusText = exception.statusText,
+                errorCode = error.errorCode,
+                errorMessage = error.errorMessage
+            )
+        )
+
     private fun applyDefaults(result: ResultForOneInstance, defaults: AlibeezInstanceDefaults) =
         ResultForOneInstance(
             employees = result.employees.map {
@@ -81,6 +89,27 @@ class CurrentEmployeesResource(
                 )
             }
         )
+}
+
+@Service
+class AlibeezCurrentEmployeesFetcher(
+    restTemplateBuilder: RestTemplateBuilder,
+    private val objectMapper: ObjectMapper
+) {
+    private val restTemplate = restTemplateBuilder.build()
+
+    fun fetch(
+        uri: URI,
+        mapSuccess: (success: AlibeezResponse) -> ResultForOneInstance,
+        mapError: (exception: HttpClientErrorException, error: AlibeezError) -> ResultForOneInstance
+    ): ResultForOneInstance =
+        try {
+            val response = restTemplate.getForObject(uri, AlibeezResponse::class.java)
+            mapSuccess(response)
+        } catch (ex: HttpClientErrorException) {
+            val error = objectMapper.readValue(ex.responseBodyAsString, AlibeezError::class.java)
+            mapError(ex, error)
+        }
 }
 
 data class AlibeezResponse(
