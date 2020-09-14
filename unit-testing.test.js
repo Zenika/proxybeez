4ab2afa07f9f61613a16cbb57d4496eb7abf406d
Ked57 <kedketh@gmail.com>
2020-09-14 12:31:50 +0200

    add prettier, add CI and deployment

--- unit-testing.test.js
+++ unit-testing.test.js
@@ -1,13 +1,16 @@
 import { parseAlibeezParamsFromQuery } from "./src/utils.js";
 
 const isQueryEscapedCorrectly = () => {
-  const query = "username=dreamlab@zenika.com"
-  const result = parseAlibeezParamsFromQuery(query)
-  console.assert(result.username === "dreamlab%40zenika.com", "Error for test isQueryEscapedCorrectly")
+  const query = "username=dreamlab@zenika.com";
+  const result = parseAlibeezParamsFromQuery(query);
+  console.assert(
+    result.username === "dreamlab%40zenika.com",
+    "Error for test isQueryEscapedCorrectly"
+  );
 
-  const query2 = "username=dreamlab@zenika.com&filter=lastName==dreamlab"
-  const result2 = parseAlibeezParamsFromQuery(query)
-  console.assert(result2.username === "dreamlab%40zenika.com")
-}
+  const query2 = "username=dreamlab@zenika.com&filter=lastName==dreamlab";
+  const result2 = parseAlibeezParamsFromQuery(query);
+  console.assert(result2.username === "dreamlab%40zenika.com");
+};
 
-isQueryEscapedCorrectly()
+isQueryEscapedCorrectly();
