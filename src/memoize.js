678d5cf3ec224b8a6093cdcd9289c1654e8b2d44
Ked57 <kedketh@gmail.com>
2020-09-16 10:56:04 +0200

    add delete mechanism for memoization cache

--- src/memoize.js
+++ src/memoize.js
@@ -1,4 +1,4 @@
-const CACHE_TTL = process.env.CACHE_TTL || 60 * 60 * 1000;
+const CACHE_TTL = Number(process.env.CACHE_TTL || 60 * 60 * 1000);
 
 const cache = new Map();
 
@@ -8,6 +8,9 @@ export const memoize = (func) => {
     const key = JSON.stringify(args);
     if (!cache.get(key)) {
       cache.set(key, { result: await func(args), date: now });
+      setTimeout(() => {
+        cache.delete(key)
+      }, CACHE_TTL + 1000);
       return cache.get(key).result;
     }
     if (now - cache.get(key).date >= CACHE_TTL) {
