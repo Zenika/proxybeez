f81443ab9cb532556240fee5849dbe9d296c60ac
Hugo Wood <hugo.wood@zenika.com>
2020-09-23 15:58:58 +0200

    tests for exclude fields

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import { renderOutgoingUrl } from "./server.js";
-import { insertKey } from "./request-processors.js";
+import { excludeFields, insertKey } from "./request-processors.js";
 import { defaultFields } from "./response-processors.js";
 
 (() => {
@@ -40,6 +40,41 @@ import { defaultFields } from "./response-processors.js";
     actual,
     expected,
     "'defaultFields' overrides fields that already have a value"
+  );
+})();
+
+(() => {
+  const actual = excludeFields("http://example.com?fields=excluded", [
+    "excluded",
+  ]);
+  assert.doesNotMatch(
+    actual.searchParams.get("fields"),
+    /excluded/,
+    "'excludeFields' cannot exclude the only field"
+  );
+})();
+
+(() => {
+  const actual = excludeFields(
+    "http://example.com?fields=included,excluded,alsoIncluded",
+    ["excluded"]
+  );
+  assert.doesNotMatch(
+    actual.searchParams.get("fields"),
+    /excluded/,
+    "'excludeFields' cannot exclude one field among multiple"
+  );
+})();
+
+(() => {
+  const actual = excludeFields(
+    "http://example.com?fields=included,alsoIncluded",
+    ["excluded"]
+  );
+  assert.match(
+    actual.searchParams.get("fields"),
+    /included,alsoIncluded/,
+    "'excludeFields' excludes fields that it should not"
   );
 })();
 
