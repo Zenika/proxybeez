e3bea523bdec6451dacee05b9dfa9d185e75608e
Hugo Wood <hugo.wood@zenika.com>
2020-10-08 11:06:28 +0200

    🛠 support config file
    A config file is more convenient in dev than a big json string in .env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+import * as fs from "fs";
 import { typecheckConfig } from "./config.js";
 import { createServer } from "./server.js";
 
 const PORT = process.env.PORT || 3000;
-const CONFIG = parseConfig(process.env.PROXYBEEZ_CONFIG);
+const CONFIG = readConfig(
+  process.env.PROXYBEEZ_CONFIG,
+  process.env.PROXYBEEZ_CONFIG_FILE
+);
 
 createServer(CONFIG).listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+/**
+ *
+ * @param {string | undefined} envConfig
+ * @param {string | undefined} fileConfig
+ */
+function readConfig(envConfig, fileConfig) {
+  if (envConfig) {
+    return parseConfig(envConfig);
+  }
+  if (!fileConfig) {
+    throw new Error(
+      `environment variable PROXYBEEZ_CONFIG and PROXYBEEZ_CONFIG_FILE are both empty; please set either one`
+    );
+  }
+  let fileContent;
+  try {
+    fileContent = fs.readFileSync(fileConfig).toString();
+  } catch (err) {
+    throw new Error(
+      `environment variable PROXYBEEZ_CONFIG is empty and PROXYBEEZ_CONFIG_FILE is '${fileConfig}' which does not point to a file that could be read: ${err.message}`
+    );
+  }
+  return parseConfig(fileContent);
+}
 
 /**
  *
