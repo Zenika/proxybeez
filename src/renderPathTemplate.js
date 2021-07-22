37a282d8bba6f1078655234c25a158813fc687e2
Hugo Wood <hugo.wood@zenika.com>
2021-07-23 00:35:48 +0200

    🐛 fix server error when missing param
    Server was responding 500 instead of 400.

--- src/renderPathTemplate.js
+++ src/renderPathTemplate.js
@@ -9,8 +9,9 @@ import * as querystring from "querystring";
 export default function renderPathTemplate(template, searchParams) {
   return template.replace(/\${(.*?)}/g, (_, $1) => {
     if (!searchParams.has($1)) {
-      throw new TypeError(
-        `Cannot render template: missing value for key '${$1}'`
+      throw Object.assign(
+        new Error(`Cannot render template: missing value for key '${$1}'`),
+        { key: $1 }
       );
     }
     return querystring.escape(searchParams.get($1) ?? "");
